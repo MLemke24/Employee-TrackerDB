c8136212af3e1c65d4048b72cc9a9ef5b93c70b7
mlemke24 <mattrlemke@gmail.com>
2021-04-28 16:19:24 -0500

    index done, just need video and readme

--- db/seeds.sql
+++ db/seeds.sql
@@ -11,7 +11,7 @@ VALUES
 ('Lead Engineer', 145000, 1),
 ('Engineer', 100000, 1), 
 ('Lead Office Manager', 145000, 2),
-('Accounting', 80000, 2),
+('Accountant', 80000, 2),
 ('Sales', 78000, 3),
 ('Facilities', 45000, 4),
 ('Lead Human Resources', 145000, 5),
@@ -19,6 +19,17 @@ VALUES
 
 INSERT INTO employees (first_name, last_name, manager_id, role_id)
 VALUES
-('John', 'Doe', null, 1),
-('Matt', 'Lemke', 1, 2);
+('Kobe', 'Bryant', null, 1),
+('Pau', 'Gasol', 1, 2),
+('Andrew', 'Bynum', 1, 2),
+('LeBron', 'James', null, 3),
+('Tyler', 'Johnson', 4, 4),
+('Kevin', 'Durant', 4, 4),
+('Magic', 'Johnson', 4, 5),
+('Chris', 'Paul', 4, 5),
+('Devin', 'Booker', 4, 6),
+('Shaquille', 'ONeal', 4, 6),
+('Steph', 'Curry', null, 7),
+('Klay', 'Thompson', 11, 8),
+('Draymond', 'Green', 11, 8);
 
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const database = () => {
             type: 'list',
             name: 'action',
             message: 'what would you like to do?',
-            choices: ['See all employees', 'See employees by department', 'View all employees by manager', 'Add employee', 'Remove Employee', 'Update Employee'] 
+            choices: ['See all employees', 'See employees by department', 'View all employees by manager', 'Add employee', 'Remove employee', 'Update role'] 
         }
     ])
     .then (data => {
@@ -27,13 +27,126 @@ const database = () => {
                     database()
                   }
             )
+        } else if (data.action == 'See employees by department') {
+            db.query (
+                `SELECT e.first_name AS "First Name", e.last_name AS "Last Name", r.title AS "Title", d.name AS "Department"
+                FROM employees e 
+                INNER JOIN roles r 
+                ON r.id = e.role_id 
+                INNER JOIN department d
+                ON d.id = r.department_id
+                ORDER BY r.department_id DESC;;`,
+                function(err, res) {
+                    console.table(res)
+                    database()
+                }
+            )
+        } else if (data.action == 'View all employees by manager'){
+            db.query (
+                `SELECT e.id, e.first_name AS "First Name", e.last_name AS "Last Name", r.title AS "Title", CONCAT(m.first_name," ",m.last_name) AS "Manager"
+                FROM employees e
+                LEFT JOIN roles r
+                ON r.id = e.role_id 
+                LEFT JOIN department d
+                ON d.id = r.department_id
+                LEFT JOIN employees m ON m.id = e.manager_id
+                ORDER BY e.manager_id DESC;`,
+                function(err, res) {
+                    console.table(res)
+                    database()
+                }
+            )
+        } else if (data.action == 'Add employee') {
+            addEmployee();
+        } else if (data.action == 'Remove employee') {
+            deleteEmployee()
+        } else if (data.action == 'Update role'){
+            updateRole()
         }
     })
 }
 database()
 
-// employees.first_name, employees.last_name, roles.title, roles.salary, department.name, AS Manager FROM employee INNER JOIN role on role.id = employee.roles_id INNER JOIN department on department.id = roles.department_id LEFT JOIN employees on employees.manager_id = employees.id
 
+function addEmployee() {
+    inquirer.prompt([
+        {
+            name: 'first_name',
+            type: 'input',
+            message: 'What is their first name?'
+        },
+        {
+            name: 'last_name',
+            type: 'input',
+            message: 'What is their last name?'
+        },
+        {
+            name: 'manager_id',
+            type: 'list',
+            message: "What is their manager id?",
+            choices: ['1', '4', '11', 'NULL']
+        },
+        {
+            name: 'role_id',
+            type: 'list',
+            message: 'What is their role id?',
+            choices: ['1', '2', '3', '4', '5', '6', '7', '8']
+        }
+    ]).then(data => {
+        const sql = `INSERT INTO employees (first_name, last_name, manager_id, role_id) VALUES (?,?,?,?)`
+        const params = [data.first_name, data.last_name, data.manager_id, data.role_id]
+        db.query(sql,params, (err, result) => {
+              if(err) throw err;
+              console.log('employee was successfully added')
+              database()
+        })
+     
+    })
+}
+
+function deleteEmployee() {
+    inquirer.prompt([
+        {
+            name: 'employees.id',
+            type: 'input',
+            message: 'What is the employees ID that you would like to delete?'
+            }
+        ]).then(data => {
+            const sql = `DELETE FROM employees WHERE id = ?`
+            const params = [data.employees.id]
+            db.query(sql,params, (err, result) => {
+                if(err) throw err;
+                console.log('employee was successfully deleted')
+                database()
+          })
+        })
+}
+
+function updateRole() {
+    inquirer.prompt([
+        {
+            name: 'roles.id',
+            type: 'input',
+            message: 'What is the employees ID?'
+        },
+        {
+            name: 'role_id',
+            type: 'input',
+            message: 'What would you like their new role ID to be?'
+        }
+    ]).then(data => {
+        sql = `UPDATE employees SET role_id = ?
+        WHERE id = ?`
+        const params = [data.role_id, data.roles.id]
+        db.query(sql,params, (err, result) => {
+            if(err) throw err;
+            console.log('employees role was successfully updated')
+            database()
+      })
+    })
+}
+
+// Get all employees
 // SELECT e.id, e.first_name AS "First Name", e.last_name AS "Last Name", r.title AS "Title", d.name AS "Department", r.salary AS "Salary", CONCAT(m.first_name," ",m.last_name) AS "Manager"
 // FROM employees e
 // LEFT JOIN roles r
@@ -42,3 +155,23 @@ database()
 // ON d.id = r.department_id
 // LEFT JOIN employees m ON m.id = e.manager_id
 // ORDER BY e.id;
+
+// get all roles
+// SELECT e.first_name AS "First Name", e.last_name AS "Last Name", r.title AS "Title", d.name AS "Department"
+// FROM employees e 
+// INNER JOIN roles r 
+// ON r.id = e.role_id 
+// INNER JOIN department d
+// ON d.id = r.department_id
+// ORDER BY r.department_id DESC;
+
+// get employees by manager
+
+// SELECT e.id, e.first_name AS "First Name", e.last_name AS "Last Name", r.title AS "Title", CONCAT(m.first_name," ",m.last_name) AS "Manager"
+// FROM employees e
+// LEFT JOIN roles r
+// ON r.id = e.role_id 
+// LEFT JOIN department d
+// ON d.id = r.department_id
+// LEFT JOIN employees m ON m.id = e.manager_id
+// ORDER BY e.manager_id DESC;
